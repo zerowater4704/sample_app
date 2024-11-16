dfb1ef185e7b5b0de318b3cb498520a3869a8c28
EC2 Default User <ec2-user@ip-172-31-33-74.eu-north-1.compute.internal>
2024-11-16 09:12:54 +0000

    [Finish] 3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,9 +30,16 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+
+    redirect_to lists_path
+  end
+
   private 
   
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
